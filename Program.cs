6c8c30dd2ae6f9c4450181600eecdd380fde9449
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-18 01:01:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_ASCII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                char c;
+                Console.WriteLine("Enter a character: ");
+                c = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("\nASCII Value of " + c + " " + Convert.ToInt32(c));
+                Console.ReadKey();
+            }
+        }
+    }
+}
+       
+        
+
+    
+    
+
